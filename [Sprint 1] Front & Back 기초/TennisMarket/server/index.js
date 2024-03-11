e5673bcadd42821a97fd48fdc89b547dc8c3e7a1
JIMIN1020 <jimins4920@gmail.com>
2024-03-11 15:27:55 +0900

    deploy

--- [Sprint 1] Front & Back 기초/TennisMarket/server/index.js
+++
@@ -1,9 +0,0 @@
-const server = require("./server");
-const router = require("./router");
-const requestHandler = require("./requestHandler");
-
-// db 연결
-const mariadb = require("./database/connect/mariadb");
-mariadb.connect();
-
-server.start(router.route, requestHandler.handle);
