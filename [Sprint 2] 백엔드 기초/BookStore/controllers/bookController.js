344dca94c85ce4bc0dfb6b2f4791ea5c9f7b9d55
JIMIN1020 <jimins4920@gmail.com>
2024-04-12 11:39:50 +0900

    [Sprint 2] BookStore 프로젝트 | 페이징 처리

--- [Sprint 2] 백엔드 기초/BookStore/controllers/bookController.js
+++ [Sprint 2] 백엔드 기초/BookStore/controllers/bookController.js
@@ -1,26 +1,39 @@
+// 4월 12일 과제 - 류지민
+
 const bookService = require("../services/bookService");
 const bookQuery = require("../queries/bookQuery");
 const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../CustomError");
 
 /* ----- 도서 목록 조회 ----- */
 const getAllBooks = async (req, res) => {
-  let { categoryId, newly } = req.query;
-
-  // sql 구하기 - 전체 / 카테고리별 / 카테고리별 신간 / 전체 신간
-  let sql = bookQuery.getAllBook;
-  let values = [];
-
-  if (categoryId && newly) {
-    sql += bookQuery.getNewlyByCategory;
-    values.push(categoryId);
-  } else if (categoryId) {
-    sql += bookQuery.getAllByCategory;
-    values.push(categoryId);
-  } else if (newly) {
-    sql += bookQuery.getAllNewly;
-  }
+  let { limit, page, categoryId, newly } = req.query;
 
   try {
+    // sql 구하기 - 전체 / 카테고리별 / 카테고리별 신간 / 전체 신간
+    let sql = bookQuery.getAllBook;
+    let values = [];
+
+    if (categoryId && newly) {
+      sql += bookQuery.getNewlyByCategory;
+      values.push(categoryId);
+    } else if (categoryId) {
+      sql += bookQuery.getAllByCategory;
+      values.push(categoryId);
+    } else if (newly) {
+      sql += bookQuery.getAllNewly;
+    }
+
+    // paging 처리
+    if (!limit || !page) {
+      throw new CustomError(
+        StatusCodes.BAD_REQUEST,
+        "요청 값을 다시 확인해주세요."
+      );
+    } else {
+      sql += ` LIMIT ${+limit} OFFSET ${(page - 1) * limit}`;
+    }
+
     const result = await bookService.getAllBooks(sql, values);
     res.status(StatusCodes.OK).json(result);
   } catch (err) {
