2957e28df1e208d97d2d55dabd4fde4f21c9686b
JIMIN1020 <jimins4920@gmail.com>
2024-04-24 13:24:22 +0900

    [Sprint 2] BookStore 프로젝트 | 쿼리 일부 수정

--- [Sprint 2] 백엔드 기초/BookStore/queries/cartQuery.js
+++ [Sprint 2] 백엔드 기초/BookStore/queries/cartQuery.js
@@ -1,4 +1,4 @@
-exports.addCartItem = `INSERT INTO cartItems (bookId, quantity, userId) VALUES (?, ?, ?);`;
-exports.getCartItems = `SELECT cartItems.id, bookId, img, title, summary, price, quantity FROM cartItems LEFT JOIN book ON cartItems.bookId = book.id WHERE userId = ?`;
+exports.addCartItem = `INSERT INTO cartItems (book_id, quantity, user_id) VALUES (?, ?, ?);`;
+exports.getCartItems = `SELECT cartItems.id, book_id, img, title, summary, price, quantity FROM cartItems LEFT JOIN book ON cartItems.book_id = book.id WHERE user_id = ?`;
 exports.getSelecItems = ` AND cartItems.id IN (?)`;
 exports.deleteCartItem = `DELETE FROM cartItems WHERE id = ?`;
--- [Sprint 2] 백엔드 기초/BookStore/queries/likeQuery.js
+++ [Sprint 2] 백엔드 기초/BookStore/queries/likeQuery.js
@@ -1,2 +1,2 @@
-exports.addLike = `INSERT INTO Likes (userId, liked_bookId) VALUES (?, ?)`;
-exports.deleteLike = `DELETE FROM Likes WHERE userId = ? AND liked_bookId = ?;`;
+exports.addLike = `INSERT INTO Likes (user_id, liked_bookId) VALUES (?, ?)`;
+exports.deleteLike = `DELETE FROM Likes WHERE user_id = ? AND liked_bookId = ?;`;
